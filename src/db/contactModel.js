8b3b662a2bb0c23f4b46dab5505c66892f5c21e8
Victoriia Kyselova <134308356+VictoriaKiselova@users.noreply.github.com>
2024-06-23 03:40:47 +0300

    add hw

--- src/db/contactModel.js
+++ src/db/contactModel.js
@@ -25,7 +25,7 @@ const contactSchema = new Schema(
       required: true,
     },
   },
-  { timestamps: true },
+  { timestamps: true, versionKey: false },
 );
 
 export const Contact = model('contacts', contactSchema);
--- src/server.js
+++ src/server.js
@@ -2,7 +2,9 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import pino from 'pino-http';
-import { getAllContacts, getContactById } from './services/contacts.js';
+import router from './routers/contacts.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
 
 dotenv.config();
 const { PORT } = process.env;
@@ -15,50 +17,17 @@ export default function setupServer() {
     },
   });
 
-  app.use(express.json());
   app.use(cors());
   app.use(logger);
+  app.use(
+    express.json({
+      type: ['application/json', 'application/vnd.api+json'],
+    }),
+  );
 
-  app.get('/contacts', async (req, res) => {
-    console.log(res);
-    try {
-      const contacts = await getAllContacts();
-      const responseData = {
-        status: 200,
-        message: 'Successfully found contacts!',
-        data: contacts,
-      };
-      res.status(200).json(responseData);
-    } catch (error) {
-      res.status(500).json({
-        status: 'error',
-        message: 'An error occurred while retrieving contacts',
-        data: null,
-      });
-    }
-  });
-
-  app.get('/contacts/:contactId', async (req, res) => {
-    const { contactId } = req.params;
-    const contact = await getContactById(contactId);
-    if (!contact) {
-      res.status(404).json({
-        message: 'Contact not found',
-      });
-      return;
-    }
-    res.status(200).json({
-      status: 200,
-      message: `Successfully found contact with id ${contactId}!`,
-      data: contact,
-    });
-  });
-
-  app.use('*', (req, res) => {
-    res.status(404).json({
-      message: 'Not found',
-    });
-  });
+  app.use('/contacts', router);
+  app.use('*', notFoundHandler);
+  app.use(errorHandler);
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
